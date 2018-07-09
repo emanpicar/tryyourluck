1efef85afafa9b28c6b956eb272ad9fbab5f33b1
Eman Picar <eman.picar@gmail.com>
2018-07-10 01:08:24 +0800

    boiletplate server added

--- server/main.go
+++ server/main.go
@@ -1,16 +1,37 @@
 package main
 
-import(
-	"fmt"
-	"net/http"
-	"log"
+import (
+  "html/template"
+  "log"
+  "net/http"
+  "time"
 )
 
-func main() {
-	http.HandleFunc("/", index_handler)
-	log.Fatal(http.ListenAndServe(":8000", nil))
+type PageVariables struct {
+	Date         string
+	Time         string
 }
 
-func index_handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "test web dev go")
+func main() {
+	http.HandleFunc("/", HomePage)
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("../client/dist/static"))))
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func HomePage(w http.ResponseWriter, r *http.Request){
+
+    now := time.Now() // find the time right now
+    HomePageVars := PageVariables{ //store the date and time in a struct
+      Date: now.Format("02-01-2006"),
+      Time: now.Format("15:04:05"),
+    }
+
+    t, err := template.ParseFiles("../client/dist/views/index.html") //parse the html file homepage.html
+    if err != nil { // if there is an error
+  	  log.Print("template parsing error: ", err) // log it
+  	}
+    err = t.Execute(w, HomePageVars) //execute the template and pass it the HomePageVars struct to fill in the gaps
+    if err != nil { // if there is an error
+  	  log.Print("template executing error: ", err) //log it
+  	}
 }
